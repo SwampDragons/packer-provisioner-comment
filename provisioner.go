63a9d12973f32e4eb8ba490adda447921dbde1be
Megan Marsh <megan@hashicorp.com>
2019-09-03 12:54:55 -0700

    make provisioner even less complex

--- provisioner.go
+++ provisioner.go
@@ -4,20 +4,14 @@ import (
 	"context"
 
 	"github.com/common-nighthawk/go-figure"
-	"github.com/hashicorp/packer/common"
 	"github.com/hashicorp/packer/helper/config"
 	"github.com/hashicorp/packer/packer"
-	"github.com/hashicorp/packer/template/interpolate"
 )
 
 type Config struct {
-	common.PackerConfig `mapstructure:",squash"`
-
 	Comment  string `mapstructure:"comment"`
 	SendToUi bool   `mapstructure:"ui"`
 	Fancy    bool   `mapstructure:"fancy"`
-
-	ctx interpolate.Context
 }
 
 type CommentProvisioner struct {
@@ -26,8 +20,7 @@ type CommentProvisioner struct {
 
 func (p *CommentProvisioner) Prepare(raws ...interface{}) error {
 	err := config.Decode(&p.config, &config.DecodeOpts{
-		Interpolate:        true,
-		InterpolateContext: &p.config.ctx,
+		Interpolate: true,
 	}, raws...)
 	if err != nil {
 		return err
@@ -36,7 +29,7 @@ func (p *CommentProvisioner) Prepare(raws ...interface{}) error {
 	return nil
 }
 
-func (p *CommentProvisioner) Provision(_ context.Context, ui packer.Ui, comm packer.Communicator) error {
+func (p *CommentProvisioner) Provision(_ context.Context, ui packer.Ui, _ packer.Communicator) error {
 	if p.config.SendToUi {
 		if p.config.Fancy {
 			myFigure := figure.NewFigure(p.config.Comment, "", false)
