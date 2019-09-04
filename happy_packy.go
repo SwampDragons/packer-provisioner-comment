d1b49b4f9de366750efefb2bc957e622a62f9dff
Megan Marsh <megan@hashicorp.com>
2019-09-04 13:17:21 -0700

    add packersay option and rename fancy to bubble

---
+++ happy_packy.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"text/template"
+)
+
+type CommentFormatter struct {
+	Comment string
+}
+
+func CreatePackerFriend(comment string) (string, error) {
+	// TODO: Make packer-friend resilient to long text strings that need to
+	// be broken into multiple lines.
+
+	PackerSay := `
+      \
+\    |  \
+| \  |    \
+|   \ \     \
+|     \ \     \
+|       \ \     \
+|        | | ◕  ◕*
+|        | |      |
+|        | |      |
+|        | |    o |
+|        | |      |   *{{.Comment}}*
+|        | |   ヮ /  /
+|        |  -___-
+\        |
+  \      |
+    \    |
+      \  |
+        \|
+`
+
+	tpl := template.Must(template.New("createVM").Parse(PackerSay))
+	var b bytes.Buffer
+	c := CommentFormatter{comment}
+
+	err := tpl.Execute(&b, c)
+	if err != nil {
+		return "", err
+	}
+
+	return b.String(), nil
+}
--- provisioner.go
+++ provisioner.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/common-nighthawk/go-figure"
 	"github.com/hashicorp/packer/helper/config"
@@ -9,9 +10,10 @@ import (
 )
 
 type Config struct {
-	Comment  string `mapstructure:"comment"`
-	SendToUi bool   `mapstructure:"ui"`
-	Fancy    bool   `mapstructure:"fancy"`
+	Comment   string `mapstructure:"comment"`
+	SendToUi  bool   `mapstructure:"ui"`
+	Bubble    bool   `mapstructure:"bubble_text"`
+	PackerSay bool   `mapstructure:"packer_say"`
 }
 
 type CommentProvisioner struct {
@@ -26,17 +28,29 @@ func (p *CommentProvisioner) Prepare(raws ...interface{}) error {
 		return err
 	}
 
+	if p.config.PackerSay && p.config.Bubble {
+		return fmt.Errorf("Can't have both packer_say and bubble_text options set.")
+	}
+
 	return nil
 }
 
 func (p *CommentProvisioner) Provision(_ context.Context, ui packer.Ui, _ packer.Communicator) error {
 	if p.config.SendToUi {
-		if p.config.Fancy {
+		if p.config.Bubble {
 			myFigure := figure.NewFigure(p.config.Comment, "", false)
 			ui.Say(myFigure.String())
+		} else if p.config.PackerSay {
+			// CreatePackerFriend is being imported from happy_packy.go
+			packyText, err := CreatePackerFriend(p.config.Comment)
+			if err != nil {
+				return err
+			}
+			ui.Say(packyText)
 		} else {
 			ui.Say(p.config.Comment)
 		}
+
 	}
 
 	return nil
